2824e6949f91ad03b7303ab3342e0df365c5216d
jayavinesh77 <155451048+jayavinesh77@users.noreply.github.com>
2024-02-22 14:24:25 +0530

    Update BACKEND.TF

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
-  backend "s3" {
+  backend "S3" {
     bucket         = "tetris-bucket"
-    region         = "us-east-1"
+    region         = "ap-south-1"
     key            = "EKS-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
@@ -9,7 +9,7 @@ terraform {
   required_version = ">=0.13.0"
   required_providers {
     aws = {
-      version = ">= 2.7.0"
+      version = ">= 1.7.3"
       source  = "hashicorp/aws"
     }
   }
